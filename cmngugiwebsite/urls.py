783b10343ab4c70aa20c9f273ecfb228d7100d52
Dennis Mwangi <denixxdenixx64@gmail.com>
2023-09-14 12:08:23 +0300

    all changes

--- cmngugiwebsite/urls.py
+++ cmngugiwebsite/urls.py
@@ -5,7 +5,5 @@ urlpatterns = [
     path('',views.index, name='index' ),
     path('about',views.about, name='about' ),
     path('contact',views.contact, name='contact' ),
-    path('services',views.services, name='services' ),
-    path('blog',views.blog, name='blog' ),
-    path('blogpost/<int:blog_id>',views.blogpost, name='blogpost' )
+    path('services',views.services, name='services' )
 ]
--- cmngugiwebsite/views.py
+++ cmngugiwebsite/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-from .models import Blog
+
 
 # Create your views here.
 def index(request):
     '''get the latest 3 blogs'''
-    blogs = Blog.objects.all().order_by('-id')[:3]
-    context = {
-        'blogs':blogs
-    }
-    return render(request, 'index.html',context)
+   
+    return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about-us.html') 
@@ -19,19 +16,5 @@ def contact(request):
 def services(request):
     return render(request, 'service.html')
 
-def blog(request):
-    blogs = Blog.objects.all()
-    context = {
-        'blogs':blogs
-    }
-    return render(request, 'blog.html',context)
 
-def blogpost(request, blog_id):
-    blog = Blog.objects.get(id=blog_id)
-    all_blogs = Blog.objects.all()
-    context = {
-        'blog':blog,
-        'all_blogs':all_blogs
-    }
-    return render(request, 'single-blog.html',context)
 
